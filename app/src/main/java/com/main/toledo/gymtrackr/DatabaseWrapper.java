84fe2e5a2088e813d451218ad385c724b8b14f68
kodieGlosser <kodieglosser@gmail.com>
2015-02-26 15:26:55 -0500

    Made some more DB changes

--- app/src/main/java/com/main/toledo/gymtrackr/DatabaseWrapper.java
+++ app/src/main/java/com/main/toledo/gymtrackr/DatabaseWrapper.java
@@ -29,7 +29,7 @@ public class DatabaseWrapper {
     private static final String COLUMN_ID = "_ID";
     private static final String EXERCISE_TABLE = "Exercises";
     private static final String EXERCISE_HISTORY_TABLE = "History";
-    private static final String DATE_FORMAT = "dd/mm/yyyy HH:mm:ss";
+    private static final String DATE_FORMAT = "YYYY-MM-DD HH:MM:SS";
     private static String DB_PATH = "/data/data/com.main.toledo.gymtrackr/databases/";
     private static String DB_NAME = "gymtrackr.db";
     private SQLiteDatabase myDatabase;
@@ -135,8 +135,11 @@ public class DatabaseWrapper {
      * This will load the history of the exercises sorted by date and name
      * @return ArrayList of the exercise history object
      */
-    public ExerciseHistory[] loadHistoryExerciseNames() {
-        return null;
+    public Exercise[] loadHistoryExerciseNames() {
+        String rawQuery = "select Exercises.name from Exercises where Exercises._id IN (select History.exercise from History) ORDER BY Exercises.name";
+        Cursor c = myDatabase.rawQuery(rawQuery, null);
+
+        return convertCursorToExercises(c);
     }
 
 
@@ -146,7 +149,7 @@ public class DatabaseWrapper {
      * @return
      */
     public ExerciseHistory[] loadHistoryByExerciseName(String exerciseName) {
-        String rawQuery = "select * from History where History.exercise IN (select Exercises._id from Exercises where Exercises.name=?) ORDER BY datetime(History.date) DESC Limit 1";
+        String rawQuery = "select * from History where History.exercise IN (select Exercises._id from Exercises where Exercises.name=?) ORDER BY datetime(History.date) DESC";
         String[] selectionArgs = new String[] { exerciseName };
 
         Cursor c = myDatabase.rawQuery(rawQuery, selectionArgs);
@@ -154,73 +157,38 @@ public class DatabaseWrapper {
         return convertCursorToExerciseHistory(c);
     }
 
-    private ExerciseHistory[] convertCursorToExerciseHistory(Cursor c) {
-        int count = c.getCount();
-        int i = 0;
-        ExerciseHistory[] exerciseHistory= new ExerciseHistory[count];
-
-        if(count >= 1) {
-            while (c.moveToNext()) {
-                String s_val_date; int val_weight= -1, val_rep= -1, val_exerciseId = -1, val_planId = -1, columnData = -1;
-                Date val_date = null;
-                for (int j = 0; j < 5; j++) {
-
-                    String columnName = c.getColumnName(j);
-
-                    if (!columnName.equalsIgnoreCase(COLUMN_DATE))
-                        columnData = c.getInt(c.getColumnIndex(columnName));
-
-
-                    if (columnName.equalsIgnoreCase(COLUMN_DATE)) {
-                        s_val_date = c.getString(c.getColumnIndex(columnName));
-
-                        DateFormat format = new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH);
-                        try {
-                            val_date = format.parse(s_val_date);
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    else if (columnName.equalsIgnoreCase(COLUMN_WEIGHT)){
-                         val_weight = columnData;
-                    }
-                    else if (columnName.equalsIgnoreCase(COLUMN_REP)){
-                        val_rep = columnData;
-                    }
-                    else if (columnName.equalsIgnoreCase(COLUMN_EXERCISE)){
-                        val_exerciseId = columnData;
-                    }
-                    else if (columnName.equalsIgnoreCase(COLUMN_PLAN)) {
-                        val_planId = columnData;
-                    }
-                }
-
-                exerciseHistory[i] = new ExerciseHistory(val_date, val_weight, val_rep, val_exerciseId, val_planId);
-                i++;
-            }
-        }
-
-        return exerciseHistory;
+    public void addCircuitToPlan(int sequence, int planNumber, String circuitName) {
+        // query: insert into Planned_Union (plannedWorkout, plan, circuitName) values (sequence, planNumber, circuitName)
     }
 
     /**
-     * Stores the exercise into the plan
+     * Adds the exercise to the Circuit
      * @param exercise the exercise to be added
      * @param circuitNumber the circuit number where its added
-     * @param planNumber the plan number where its added
+     * @param sequence the sequence inside the circuit
+     * @param isItOpen true if open, false if not
      */
-    public void addExerciseToPlan(Exercise exercise, int circuitNumber, int planNumber) {
+    public void addExerciseToCircuit(Exercise exercise, int circuitNumber, int sequence, boolean isItOpen, int planId) {
 
     }
 
     /**
      * If we want to delete it then we can just set the new position and new circuit to -1.
      * @param oldCircuitNumber
-     * @param oldPosition
+     * @param oldSequence
      * @param newCircuit
-     * @param newPosition
+     * @param newSequence
      */
-    public void alterExerciseInPlan(int oldCircuitNumber, int oldPosition, int newCircuit, int newPosition) {
+    public void alterExerciseInPlan(int oldCircuitNumber, int oldSequence, int newCircuit, int newSequence, int planId) {
+
+    }
+
+    /**
+     * Passing -1 will delete the record
+     * @param oldSequence
+     * @param newSequence
+     */
+    public void alterCircuitInPlan(int oldSequence, int newSequence, int planId){
 
     }
 
@@ -267,14 +235,52 @@ public class DatabaseWrapper {
         return exercises;
     }
 
-    public void storeExerciseIntoCircuit(Exercise[] exercises, int circuitNumber){
+    private ExerciseHistory[] convertCursorToExerciseHistory(Cursor c) {
+        int count = c.getCount();
+        int i = 0;
+        ExerciseHistory[] exerciseHistory= new ExerciseHistory[count];
 
+        if(count >= 1) {
+            while (c.moveToNext()) {
+                String s_val_date; int val_weight= -1, val_rep= -1, val_exerciseId = -1, val_planId = -1, columnData = -1;
+                Date val_date = null;
+                for (int j = 0; j < 5; j++) {
+
+                    String columnName = c.getColumnName(j);
+
+                    if (!columnName.equalsIgnoreCase(COLUMN_DATE))
+                        columnData = c.getInt(c.getColumnIndex(columnName));
+
+
+                    if (columnName.equalsIgnoreCase(COLUMN_DATE)) {
+                        s_val_date = c.getString(c.getColumnIndex(columnName));
+
+                        DateFormat format = new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH);
+                        try {
+                            val_date = format.parse(s_val_date);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if (columnName.equalsIgnoreCase(COLUMN_WEIGHT)){
+                        val_weight = columnData;
+                    }
+                    else if (columnName.equalsIgnoreCase(COLUMN_REP)){
+                        val_rep = columnData;
+                    }
+                    else if (columnName.equalsIgnoreCase(COLUMN_EXERCISE)){
+                        val_exerciseId = columnData;
+                    }
+                    else if (columnName.equalsIgnoreCase(COLUMN_PLAN)) {
+                        val_planId = columnData;
+                    }
+                }
+
+                exerciseHistory[i] = new ExerciseHistory(val_date, val_weight, val_rep, val_exerciseId, val_planId);
+                i++;
+            }
+        }
+
+        return exerciseHistory;
     }
-
-    public void storeCircuitIntoWorkout(ArrayList<Circuit> circuit, int workoutNumber) {
-
-    }
-
-
-
 }
