5495125a37d0f74746ad5f849afc7631a245e082
kodieGlosser <kodieglosser@gmail.com>
2015-03-12 21:58:09 -0400

    fixed delete db issue

--- app/src/main/java/com/main/toledo/gymtrackr/DatabaseWrapper.java
+++ app/src/main/java/com/main/toledo/gymtrackr/DatabaseWrapper.java
@@ -325,9 +325,12 @@ public class DatabaseWrapper {
         Cursor c = myDatabase.query(COLUMN_PLAN, new String[] { COLUMN_ID }, COLUMN_NAME + "=? COLLATE NOCASE", new String[] { planName}, null, null, null);
         int planId = -1;
 
-        while (c.moveToNext()) {
-            planId = c.getInt(c.getColumnIndex(COLUMN_ID));
+        if (c.getCount() >= 1) {
+            while (c.moveToNext()) {
+                planId = c.getInt(c.getColumnIndex(c.getColumnName(0)));
+            }
         }
+
         return planId;
     }
 
