124fc48a7cb981fca8a2576cf181eac9f419d69e
sremmustap <a.servis@hotmail.com>
2015-03-31 13:23:33 -0700

    Swiping up and down in the detail view will navigate to next/previous item on workspace.

--- app/src/main/java/com/main/toledo/gymtrackr/EditExerciseDetailsFragment.java
+++ app/src/main/java/com/main/toledo/gymtrackr/EditExerciseDetailsFragment.java
@@ -30,6 +30,9 @@ public class EditExerciseDetailsFragment extends Fragment {
     private Exercise exercise;
     private Circuit circuit;
     private String title;
+
+    private LinearLayout editTextLayout;
+    private TextView exerciseInfoTextView;
     private ArrayList<EditText> editList = new ArrayList<>();
 
     @Override
@@ -48,17 +51,190 @@ public class EditExerciseDetailsFragment extends Fragment {
 
         //sets the view for the fragment
         View v = inflater.inflate(R.layout.e_frag_details, null);
-        LinearLayout layout = (LinearLayout) v.findViewById(R.id.detailLinearLayout);
+
+        editTextLayout = (LinearLayout) v.findViewById(R.id.detailLinearLayout);
+        exerciseInfoTextView = (TextView) v.findViewById(R.id.exerciseNameView);
+
+        implementSwipeListener(v);
+        updateUI();
+
+        return v;
+    }
+
+    private void implementSwipeListener(View v){
+        RelativeLayout layout = (RelativeLayout) v.findViewById(R.id.detailView);
+
+        layout.setOnTouchListener(new View.OnTouchListener() {
+            private int swipeThreshold = 150;
+            private float currentY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int action = event.getAction();
+
+                switch (action){
+                    case MotionEvent.ACTION_DOWN: {
+                        Log.d("DETAIL SWIPE TESTS", "ACTION_DOWN");
+                        final int pointerIndex = MotionEventCompat.getActionIndex(event);
+                        final float startY = MotionEventCompat.getY(event, pointerIndex);
+
+                        v.postDelayed(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                if(currentY > startY + swipeThreshold){
+                                    //swipe down (NEXT)
+                                    next();
+                                    Log.d("DETAIL SWIPE TESTS", "NEXT");
+                                }
+                                if(currentY < startY - swipeThreshold){
+                                    previous();
+                                    Log.d("DETAIL SWIPE TESTS", "PREVIOUS");
+                                }
+
+                            }
+                        }, 500);
+
+
+
+
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+
+                        currentY = event.getY();
+                        Log.d("DETAIL SWIPE TESTS", "ACTION_MOVE" + currentY);
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_UP: {
+
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_CANCEL: {
+
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_POINTER_UP: {
+
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void next(){
+        boolean updateUI = false;
+        int numCircuitsInWorkout =
+                WorkoutData.get(getActivity()).getWorkout().size();
+
+        if(circuit.isOpen()){
+            int circuitSize = circuit.getSize();
+
+            if(exerciseValue == circuitSize - 2){ //if last exercise in open circuit
+
+                if (circuitValue == numCircuitsInWorkout - 2){ //if last circuit
+                    //last item in last circuit handled here
+                    updateUI = false;
+                }else{  //not last circuit
+                    //goto next circuit
+                    //goto first exercise
+                    circuitValue++;
+                    exerciseValue = 0;
+                    updateUI = true;
+                }
+
+            }else{ //not last exercise in open circuit
+                //
+                exerciseValue++;
+                updateUI = true;
+            }
+
+        }else{ //closed circuit
+            if(circuitValue == numCircuitsInWorkout - 2){ //last circuit
+                updateUI = false;
+            } else { //not last circuit
+                circuitValue++;
+                exerciseValue = 0;
+                updateUI = true;
+            }
+        }
+        circuit = WorkoutData.get(getActivity()).getWorkout().get(circuitValue);
+        exercise = circuit.getExercise(exerciseValue);
+        if(updateUI)
+            updateUI();
+    }
+
+    private void previous(){
+        boolean updateUI = false;
+        if(circuit.isOpen()){ //for open circuit
+            if(exerciseValue == 0){ //for first exercise
+
+                if(circuitValue == 0){ //for first circuit
+                    //first item, do nothing
+                    updateUI = false;
+
+                } else { //for not first circuit
+                    //goto last item in next circuit
+                    circuitValue--;
+                    Circuit prevCircuit =
+                            WorkoutData.get(getActivity()).getWorkout().get(circuitValue);
+
+                    if(prevCircuit.isOpen()){ //dest circuit is open
+                        exerciseValue = prevCircuit.getSize() - 2;
+                    } else { //dest circuit is closed
+                        exerciseValue = 0;
+                    }
+
+                    updateUI = true;
+                }
+
+            } else { //for not first exercise
+                exerciseValue--;
+                updateUI = true;
+            }
+        }else{ //for closed circuit
+
+            if(circuitValue == 0){//first circuit
+                updateUI = false;
+            } else { //not first circuit
+                circuitValue--;
+                Circuit prevCircuit =
+                        WorkoutData.get(getActivity()).getWorkout().get(circuitValue);
+
+                if(prevCircuit.isOpen()){ //dest circuit is open
+                    exerciseValue = prevCircuit.getSize() - 2;
+                } else { //dest circuit is closed
+                    exerciseValue = 0;
+                }
+
+                updateUI = true;
+            }
+        }
+
+        circuit = WorkoutData.get(getActivity()).getWorkout().get(circuitValue);
+        exercise = circuit.getExercise(exerciseValue);
+        if(updateUI)
+            updateUI();
+    }
+
+    private void updateUI(){
 
         if (circuit.isOpen())
-            title = exercise.getName() + " in " + circuit.getName();
+            title = exercise.getName() + " in " + circuit.getName() + " -- DEBUG -- CIRCUIT: " + circuitValue + " EXERCISE: " + exerciseValue;
         else
             title = exercise.getName();
 
-        TextView titleView = (TextView) v.findViewById(R.id.exerciseNameView);
-        titleView.setText(title);
 
-        implementSwipeListener(v);
+        exerciseInfoTextView.setText(title);
+
+        editTextLayout.removeAllViewsInLayout();
 
         for(int i = 0; i < exercise.getMetrics().size(); i++){
             switch(exercise.getMetrics().get(i).getType()){
@@ -76,7 +252,7 @@ public class EditExerciseDetailsFragment extends Fragment {
                     timeRow.addView(timeText);
                     timeRow.addView(timeEdit);
 
-                    layout.addView(timeRow);
+                    editTextLayout.addView(timeRow);
                     /*
                     LinearLayout timeLayout = (LinearLayout) v.findViewById(R.id.detailTimeViewLayout);
                     timeLayout.setVisibility(View.VISIBLE);
@@ -114,7 +290,7 @@ public class EditExerciseDetailsFragment extends Fragment {
                     repRow.addView(repText);
                     repRow.addView(repEdit);
 
-                    layout.addView(repRow);
+                    editTextLayout.addView(repRow);
                     /*
                     LinearLayout repLayout = (LinearLayout) v.findViewById(R.id.detailRepViewLayout);
                     repLayout.setVisibility(View.VISIBLE);
@@ -167,7 +343,7 @@ public class EditExerciseDetailsFragment extends Fragment {
                     wtRow.addView(wtText);
                     wtRow.addView(wtEdit);
 
-                    layout.addView(wtRow);
+                    editTextLayout.addView(wtRow);
                     /*
                     LinearLayout weightLayout = (LinearLayout) v.findViewById(R.id.detailWeightViewLayout);
                     weightLayout.setVisibility(View.VISIBLE);
@@ -195,107 +371,6 @@ public class EditExerciseDetailsFragment extends Fragment {
                     break;
             }
         }
-
-
-
-        return v;
-    }
-
-    private void implementSwipeListener(View v){
-        RelativeLayout layout = (RelativeLayout) v.findViewById(R.id.detailView);
-
-        layout.setOnTouchListener(new View.OnTouchListener() {
-            private int swipeThreshold = 150;
-            private float currentY;
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int action = event.getAction();
-
-                switch (action){
-                    case MotionEvent.ACTION_DOWN: {
-                        Log.d("DETAIL SWIPE TESTS", "ACTION_DOWN");
-                        final int pointerIndex = MotionEventCompat.getActionIndex(event);
-                        final float startY = MotionEventCompat.getY(event, pointerIndex);
-
-                        v.postDelayed(new Runnable() {
-
-                            @Override
-                            public void run() {
-
-                                if(currentY > startY + swipeThreshold){
-                                    //swipe down (NEXT)
-                                    if(circuit.isOpen()){
-                                        int circuitSize = circuit.getSize();
-
-                                        if(exerciseValue == circuitSize - 2){
-                                            int numCircuitsInWorkout =
-                                                    WorkoutData.get(getActivity()).getWorkout().size();
-                                            if (circuitSize == numCircuitsInWorkout - 2){
-                                                //last item in last circuit handled here
-                                            }else{
-                                                //goto next circuit
-                                                //goto first exercise
-                                            }
-
-                                        }else{
-
-                                        }
-
-                                    }else{
-
-                                    }
-
-                                    Log.d("DETAIL SWIPE TESTS", "NEXT");
-                                }
-
-                                if(currentY < startY - swipeThreshold){
-
-                                    if(circuit.isOpen()){
-
-                                    }else{
-
-                                    }
-
-
-
-                                    Log.d("DETAIL SWIPE TESTS", "PREVIOUS");
-                                }
-
-                            }
-                        }, 500);
-
-
-
-
-                        break;
-                    }
-
-                    case MotionEvent.ACTION_MOVE: {
-
-                        currentY = event.getY();
-                        Log.d("DETAIL SWIPE TESTS", "ACTION_MOVE" + currentY);
-                        break;
-                    }
-
-                    case MotionEvent.ACTION_UP: {
-
-                        break;
-                    }
-
-                    case MotionEvent.ACTION_CANCEL: {
-
-                        break;
-                    }
-
-                    case MotionEvent.ACTION_POINTER_UP: {
-
-                        break;
-                    }
-                }
-                return true;
-            }
-        });
-
     }
     /*
     private void set(){
