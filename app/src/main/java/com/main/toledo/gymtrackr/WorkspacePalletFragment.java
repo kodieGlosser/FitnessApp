50f21d81438a13727b6ea8c5845fd0d17dc402ab
sremmustap <a.servis@hotmail.com>
2015-05-13 15:29:42 -0700

    Item 'E' in pallet fragment spawns draggable.

--- app/src/main/java/com/main/toledo/gymtrackr/WorkspacePalletFragment.java
+++ app/src/main/java/com/main/toledo/gymtrackr/WorkspacePalletFragment.java
@@ -1,17 +1,26 @@
 package com.main.toledo.gymtrackr;
 
+import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Adam on 2/26/2015.
  */
 public class WorkspacePalletFragment extends Fragment {
 
-
+    private TextView makeExercise;
+    private ImageView exerciseDraggable;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +30,77 @@ public class WorkspacePalletFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.w_frag_pallet, null);
+        makeExercise = (TextView) v.findViewById(R.id.make_exercise);
+        makeExercise.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
+                final int action = event.getAction();
+
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN: {
+                        final float y = event.getY();
+                        final float x = event.getX();
+                        Log.d("WorkspacePalletFragment", "DOWN on E");
+
+                        WindowManager.LayoutParams mWindowParams = new WindowManager.LayoutParams();
+                        mWindowParams.x = (int)x - 700;
+                        mWindowParams.y = (int)y + 650;
+                        //TODO: MAKE SCALABLE
+                        mWindowParams.height = 200;
+                        mWindowParams.width = 200;
+                        mWindowParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                                | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+                                | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
+                        mWindowParams.format = PixelFormat.TRANSLUCENT;
+                        mWindowParams.windowAnimations = 0;
+
+                        Context context = getActivity();
+                        exerciseDraggable = new ImageView(context);
+
+                        exerciseDraggable.setImageDrawable(
+                                context.getResources().getDrawable(R.drawable.drag1));
+
+
+                        WindowManager mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+                        mWindowManager.addView(exerciseDraggable, mWindowParams);
+
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+                        if (exerciseDraggable != null) {
+                            WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) exerciseDraggable.getLayoutParams();
+                            layoutParams.x = (int)event.getX() - 700;
+                            layoutParams.y = (int)event.getY() + 650;
+                            WindowManager mWindowManager = (WindowManager) getActivity()
+                                    .getSystemService(Context.WINDOW_SERVICE);
+                            mWindowManager.updateViewLayout(exerciseDraggable, layoutParams);
+                        }
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_UP: {
+                        Log.d("WorkspacePalletFragment", "UP  on E");
+                        if (exerciseDraggable != null) {
+                            exerciseDraggable.setVisibility(View.GONE);
+                            WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+                            wm.removeView(exerciseDraggable);
+                            exerciseDraggable.setImageDrawable(null);
+                            exerciseDraggable = null;
+                        }
+
+                    }
+                }
+                return true;
+            }
+        });
         return v;
+    }
+
+    private void createExerciseDraggable(){
+
     }
 }
